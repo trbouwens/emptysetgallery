d9fbff9d0ec1651797b3d95890e54bc072d28639
Alexander Hayden <alexhayden25@gmail.com>
2021-03-22 18:43:28 -0400

    paint gallery

--- js/slogans.js
+++ js/slogans.js
@@ -1,2 +1,2 @@
-const slogans = ["Spend money.  Live better.", "Finger-lickin' good"];
+const slogans = ["Spend money.  Live better.", "Finger-lickin' good", "It's not empty unless it's {Empty Set}"];
 document.getElementById("slogan").innerText = slogans[Math.floor(Math.random() * slogans.length)];
